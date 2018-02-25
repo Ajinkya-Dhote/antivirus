91688a8af1428c5bf985d4dcb3579717005dc5b3
Ajinkya Dhote <ajinkyad@winsoftech.com>
2018-02-25 15:09:05 +0530

    Watching directoy for read, modify and delete

--- util/FileScanner/src/in/himanshudhote/FileScanner.java
+++ util/FileScanner/src/in/himanshudhote/FileScanner.java
@@ -1,12 +1,38 @@
 package in.himanshudhote;
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
 
 public class FileScanner {
 	FileScanner() {}
 	
 	void startScanning(Path dir) {
 		System.out.println(String.format("Readinding file %s", dir.toString()));
+		
+		try {
+			WatchService watcher = FileSystems.getDefault().newWatchService();
+		    WatchKey key = dir.register(watcher,
+		    		StandardWatchEventKinds.ENTRY_CREATE ,
+		    		StandardWatchEventKinds.ENTRY_DELETE,
+		    		StandardWatchEventKinds.ENTRY_MODIFY);
+
+	        while ((key = watcher.take()) != null) {
+	            for (WatchEvent<?> event : key.pollEvents()) {
+	                System.out.println(
+	                  "Event kind:" + event.kind() 
+	                    + ". File affected: " + event.context() + ".");
+	            }
+	            key.reset();
+	        }
+	        
+		} catch (IOException | InterruptedException x) {
+		    System.err.println(x);
+		}
 	}
 
 }
